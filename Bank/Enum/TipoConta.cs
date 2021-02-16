1569a96d245323480f9a3d175e16d92873faf02d
diogosalvadorb <60317215+diogosalvadorb@users.noreply.github.com>
2021-02-15 23:44:19 -0300

    classe conta

---
+++ Bank/Enum/TipoConta.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Bank
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaconta = new Conta(TipoConta.PessoaFisica, 0, 0, "Diogo");
+            Console.WriteLine(minhaconta.ToString());
+        }
+    }
+}
